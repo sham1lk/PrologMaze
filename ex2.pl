97d0086d282077ef418ccce69f683f13e67f1842
sham1lk <khastiev@gmail.com>
2020-03-09 12:57:04 +0300

    some changes

--- ex2.pl
+++ ex2.pl
@@ -1,13 +1,13 @@
 :- dynamic min/1.
 :- dynamic visited/2.
-initial_state( 0,0 ).
-f(8,5).
-f(9,9).
+:- dynamic o/2.
+:- dynamic h/2.
+:- dynamic pass/1.
+initial_state( 0,0 ).   
 
-o(0,1).
 
 
-p(1,1).
+h(1,1).
 
 % RIGHT
 update1(  X, Y, right, X_new, Y  ) :-
@@ -35,24 +35,26 @@ update4(  X,Y, up, X, Y_new  ) :-
 
 % GORESONTAL PASS
 update5(  X,Y, pass_g, X, Y_new) :-
-    p(X,Y_any),
+    h(X,Y_any),
     not(is_org_g(X,Y,Y_any)),
     Y_new is Y_any,
     legal(X,Y_new),
+    not(pass(1)),
      write("pass to ("), write(X), write(","), write(Y_new), write_ln(")").
 
 % VERTICAL PASS
 update6(  X,Y, pass_v, X_new, Y) :-
-    p(X_any,Y),
+    h(X_any,Y),
     not(is_org_v(X,X_any,Y)),
     X_new is X_any,
     legal(X_new,Y),
+    not(pass(1)),
      write("pass to ("), write(X_new), write(","), write(Y), write_ln(")").
 
 
 % DIAGONAL PASS
 update7( X,Y, pass_d, X_new,Y_new) :-
-    p(X_any,Y_any),
+    h(X_any,Y_any),
     D  is X_any - X,
     D1 is Y_any - Y,
     D=D1,
@@ -60,10 +62,11 @@ update7( X,Y, pass_d, X_new,Y_new) :-
     X_new is X_any,
     Y_new is Y_any,
     legal(X_new,Y_new),
+    not(pass(1)),
      write("pass to ("), write(X_new), write(","), write(Y_new), write_ln(")").
 
 update8( X,Y, pass_d, X_new,Y_new) :-
-    p(X_any,Y_any),
+    h(X_any,Y_any),
     D  is X_any - X,
     D1 is Y - Y_any,
     D=D1,
@@ -71,6 +74,7 @@ update8( X,Y, pass_d, X_new,Y_new) :-
     X_new is X_any,
     Y_new is Y_any,
     legal(X_new,Y_new),
+    not(pass(1)),
      write("pass to ("), write(X_new), write(","), write(Y_new), write_ln(")").
 
 
@@ -102,30 +106,34 @@ legal( X,Y ) :-
     X < 10,
     Y < 10,
     Y >= 0,
-    \+ o(X,Y),
-    \+ visited(X,Y).
+    \+ o(X,Y).
 
 move(X,Y) :-
-    f(X,Y),
+    t(X,Y),
     min(N),
     write(N).
 
 move(X,Y) :-
     min(N),
     N1 is N + 1,
-    (N>100->false;true),
+    (N>100->write("fail, more then 100 atempts"), false ; true),
     random(1,9,K),
     (K = 1 -> (update1(X,Y, M, X_new, Y_new)->assert(visited(X_new,Y_new)),retractall(min(_)),assert(min(N1)),move(X_new,Y_new); move(X,Y));true),
     (K = 2 -> (update2(X,Y, M, X_new, Y_new)->assert(visited(X_new,Y_new)),retractall(min(_)),assert(min(N1)),move(X_new,Y_new); move(X,Y));true),
     (K = 3 -> (update3(X,Y, M, X_new, Y_new)->assert(visited(X_new,Y_new)),retractall(min(_)),assert(min(N1)),move(X_new,Y_new); move(X,Y));true),
     (K = 4 -> (update4(X,Y, M, X_new, Y_new)->assert(visited(X_new,Y_new)),retractall(min(_)),assert(min(N1)),move(X_new,Y_new); move(X,Y));true),
-    (K = 5 -> (update5(X,Y, M, X_new, Y_new)->assert(visited(X_new,Y_new)),retractall(min(_)),assert(min(N1)),move(X_new,Y_new); move(X,Y));true),
-    (K = 6 -> (update6(X,Y, M, X_new, Y_new)->assert(visited(X_new,Y_new)),retractall(min(_)),assert(min(N1)),move(X_new,Y_new); move(X,Y));true),
-    (K = 7 -> (update7(X,Y, M, X_new, Y_new)->assert(visited(X_new,Y_new)),retractall(min(_)),assert(min(N1)),move(X_new,Y_new); move(X,Y));true),
-    (K = 8 -> (update8(X,Y, M, X_new, Y_new)->assert(visited(X_new,Y_new)),retractall(min(_)),assert(min(N1)),move(X_new,Y_new); move(X,Y));true).
+    (K = 5 -> (update5(X,Y, M, X_new, Y_new)-> assert(pass(1)),assert(visited(X_new,Y_new)),retractall(min(_)),assert(min(N1)),move(X_new,Y_new); move(X,Y));true),
+    (K = 6 -> (update6(X,Y, M, X_new, Y_new)-> assert(pass(1)),assert(visited(X_new,Y_new)),retractall(min(_)),assert(min(N1)),move(X_new,Y_new); move(X,Y));true),
+    (K = 7 -> (update7(X,Y, M, X_new, Y_new)-> assert(pass(1)),assert(visited(X_new,Y_new)),retractall(min(_)),assert(min(N1)),move(X_new,Y_new); move(X,Y));true),
+    (K = 8 -> (update8(X,Y, M, X_new, Y_new)-> assert(pass(1)),assert(visited(X_new,Y_new)),retractall(min(_)),assert(min(N1)),move(X_new,Y_new); move(X,Y));true).
 
 
 solve_problem :-
     set_prolog_flag(answer_write_options,[max_depth(0)]),
+    statistics(runtime, [T0|_]),
+    retractall(pass(_)),
     assert(min(0)),
-    move(0,0).
+    move(0,0),
+    statistics(runtime, [T1|_]), 
+   T is T1 - T0,
+   format('p/0 took ~3d sec.~n', [T]).
--- ex3.pl
+++ ex3.pl
@@ -2,6 +2,7 @@
 :- dynamic pass/1.
 :- dynamic o/2.
 :- dynamic h/2.
+:- dynamic jump/1.
 initial_state( 0,0 ).
 
 
@@ -23,21 +24,21 @@ update(  X,Y, up, X, Y_new  ) :-
 
 % GORESONTAL PASS
 update(  X,Y, pass_g, X, Y_new) :-
-    not(pass(1)),
+    not(pass(0)),
     h(X,Y_any),
     not(is_org_g(X,Y,Y_any)),
     Y_new is Y_any.
 
 % VERTICAL PASS
 update(  X,Y, pass_v, X_new, Y) :-
-    not(pass(1)),
+    not(pass(0)),
     h(X_any,Y),
     not(is_org_v(X,X_any,Y)),
     X_new is X_any.
 
 % DIAGONAL PASS
 update( X,Y, pass_d, X_new,Y_new) :-
-    not(pass(1)),
+    not(pass(0)),
     h(X_any,Y_any),
     D  is X_any - X,
     D1 is Y_any - Y,
@@ -47,7 +48,7 @@ update( X,Y, pass_d, X_new,Y_new) :-
     Y_new is Y_any.
 
 update( X,Y, pass_d, X_new,Y_new) :-
-    not(pass(1)),
+    not(pass(0)),
     h(X_any,Y_any),
     D  is X_any - X,
     D1 is Y - Y_any,
@@ -86,6 +87,15 @@ legal( X,Y ) :-
     Y >= 0,
     \+ o(X,Y).
 
+chek_back(X,Y,Move,X_new1, X_new2, Y_new1, Y_new2) :-
+    (Move='right'    ->X_new1 is X-1, X_new2 is X_new1, Y_new1 is Y-1, Y_new2 is Y+1;
+        (Move='left'  ->X_new1 is X+1, X_new2 is X_new1, Y_new1 is Y-1, Y_new2 is Y+1;
+            (Move='up' ->X_new1 is X+1, X_new2 is X-1, Y_new1 is Y-1, Y_new2 is Y-1;
+                (Move='down'  ->X_new1 is X+1, X_new2 is X-1, Y_new1 is Y+1, Y_new2 is Y+1;
+                    (Move='pass_d'->X_new1 is -1, X_new2 is -1, Y_new1 is -1, Y_new2 is -1;
+                        (Move='pass_v'->X_new1 is -1, X_new2 is -1, Y_new1 is -1, Y_new2 is -1;
+                         (Move='pass_g'->X_new1 is -1, X_new2 is -1, Y_new1 is -1, Y_new2 is -1;true))))))).
+
 
 dfs(X,Y, _, [],K) :-
     t(X,Y),
@@ -93,35 +103,35 @@ dfs(X,Y, _, [],K) :-
     (pass(1)->K1 is K-1;K1 is K),
     assert(min(K1)).
 
+
 dfs(X,Y, History, [[Move,(X_new,Y_new)]|Moves],K) :-
+    (X1 is X+1, t(X1,Y)-> 'right' = Move;
+        (X1 is X-1, t(X1,Y)-> 'left'=Move; 
+            (Y1 is Y-1, t(X,Y1)-> 'down'= Move;
+              (Y1 is Y+1, t(X,Y1)->'up' = Move;
+                (X1 is X+2, t(X1,Y), X2 is X+1, \+ o(X2,Y)-> 'right' = Move;
+                 (Y1 is Y+2, t(X,Y1), Y2 is Y+1, \+ o(X,Y2)-> 'up' = Move;
+                     (X1 is X-2, t(X1,Y), X2 is X-1, \+ o(X2,Y)-> 'left' = Move;
+                        (Y1 is Y-2, t(X,Y1), Y2 is Y-1, \+ o(X,Y2)-> 'down' = Move;true)))))))),
     update(X,Y, Move, X_new, Y_new),
-    legal(X_new, Y_new),
+    retractall(jump(1)),
+    legal(X_new, Y_new),    
     \+ member((X_new, Y_new), History),
     K1 is K + 1,
     min(M),
     K<M,
-    dfs(X_new, Y_new, [(X_new,Y_new)|History], Moves,K1).
+    chek_back(X,Y,Move,X_new1, X_new2 ,Y_new1,Y_new2),
 
-print([]).
-print([[Move,(X_new,Y_new)]|Moves]) :-
-    (Move = 'pass_d';Move = 'pass_v';Move = 'pass_g' -> write("P ");true),
-    write(X_new), write(" "), write(Y_new),nl,
-    print(Moves).
+    dfs(X_new, Y_new, [(X_new,Y_new),(X_new1,Y_new1),(X_new2,Y_new2)|History], Moves,K1).
 
 
-
-
-solve_problem :-
+solve_problem(Last) :-
     set_prolog_flag(answer_write_options,[max_depth(0)]),
     statistics(runtime, [T0|_]),
     assert(min(1000)),    
     initial_state(X,Y),
     findall(Solution,dfs(X,Y, [(X,Y)], Solution,0),L),
     last(L,Last),
-    length(Last,V),
-    write(V),nl,
-    print(Last),
     statistics(runtime, [T1|_]), 
-   T2 is T1 - T0,
-   T is T2*1000,
-   format('took ~3d msec.~n', [T]).
+    T is T1 - T0,
+    format('took ~3d sec.~n', [T]).
